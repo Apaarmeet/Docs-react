97f64616a506fa0b827c3e5c6723373649f2c078
apaarmeet <apaarmeet5000@gmail.com>
2024-08-10 02:01:01 +0530

    drags added

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 function Foreground() {
+
+
+    const ref = useRef(null)
+
+
+    const data = [
+        {
+        desc:"this is the background color of the display",
+        filesize:".9mb",
+        close: true,
+        tag:{isOpen: true, tagTitle:"Download Now", tagColor:"green"}
+    },
+    {
+        desc:"this is the background color of the display",
+        filesize:".9mb",
+        close: true,
+        tag:{isOpen: true, tagTitle:"upload", tagColor:"blue"}
+    },
+    {
+        desc:"this is the background color of the display",
+        filesize:".9mb",
+        close: true,
+        tag:{isOpen: false, tagTitle:"Upload", tagColor:"green"}
+    },
+];
+
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-        <Card/>
+    <div ref = {ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+        {data.map((item, index)=>(
+            <Card data={item} reference={ref} />
+        ))}
     </div>
   )
 }
