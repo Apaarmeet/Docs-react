bbb425ea5696e4617f13668d616393d0ab1c3958
apaarmeet <apaarmeet5000@gmail.com>
2024-08-09 09:29:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='relaative w-full h-screen bg-zinc-800 '>
+      <h1 className=' absolute top-1/2 left-1/2 text-[13vw] leading-none tracking-tighter -translate-x-[50%] -translate-y-[50%]'>Docs</h1>
+    </div>
+  )
+}
+
+export default App
