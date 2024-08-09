748836d68f69e8bfdc902de22d3e473f21dd24f3
apaarmeet <apaarmeet5000@gmail.com>
2024-08-09 15:38:49 +0530

    background-foreground

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Background from './components/Background';
+import Foreground from './components/Foreground';
 
 function App() {
   return (
-    <div className='relaative w-full h-screen bg-zinc-800 '>
-      <h1 className=' absolute top-1/2 left-1/2 text-[13vw] leading-none tracking-tighter -translate-x-[50%] -translate-y-[50%]'>Docs</h1>
+    <div className='relative w-full h-screen bg-zinc-800 '>
+      <Background/>
+    <Foreground/>
+
     </div>
   )
 }
